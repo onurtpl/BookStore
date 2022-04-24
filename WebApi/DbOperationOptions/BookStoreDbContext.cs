db4c1c54be5fce9897ab3ca637d76f45a63e02f7
Onur Topal <onurt.yazilim@gmail.com>
2022-04-25 00:33:34 +0300

    change branch name

--- WebApi/DbOperationOptions/BookStoreDbContext.cs
+++ WebApi/DbOperationOptions/BookStoreDbContext.cs
@@ -6,6 +6,6 @@ namespace WebApi.DbOperationOptions
     public class BookStoreDbContext : DbContext
     {
         public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options) { }
-        public DbSet<Book> Books { get; set; }
+        public DbSet<Book> Books { get; set; } 
     }
 }
