45d9baced055ff5089a6726fa55f755ac160463c
Onur Topal <onurtopal.duba@gmail.com>
2022-04-25 10:06:13 +0300

    DbContext kullanarak CRUD işlemlerinin yapılması

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.DbOperationOptions;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -10,45 +11,24 @@ namespace WebApi.Controllers
     [Route("[controller]s")]
     public class BookController : ControllerBase
     {
-        private static List<Book> BookList = new List<Book>()
+        private readonly BookStoreDbContext _context;
+
+        public BookController(BookStoreDbContext context)
         {
-            new Book
-            {
-                    Id = 1,
-                    Title = "Lean Startup",
-                    GenreId = 1,  // Personal Growth
-                    PageCount = 200,
-                    PublishDate = new DateTime(2001, 01, 21)
-            },
-            new Book
-            {
-                    Id = 2,
-                    Title = "HerLand",
-                    GenreId = 2,  // Science Finction
-                    PageCount = 250,
-                    PublishDate = new DateTime(2010, 05, 23)
-            },
-            new Book
-            {
-                    Id = 3,
-                    Title = "Dune",
-                    GenreId = 2,  // Science Finction
-                    PageCount = 540,
-                    PublishDate = new DateTime(2008, 05, 07)
-            }
-        };
+            _context = context;
+        }
 
         [HttpGet]
         public List<Book> GetBooks()
         {
-            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            var bookList = _context.Books.OrderBy(x => x.Id).ToList<Book>();
             return bookList;
         }
 
         [HttpGet("{id}")]
         public Book GetById(int id)
         {
-            var book = BookList.FirstOrDefault(x => x.Id == id);
+            var book = _context.Books.FirstOrDefault(x => x.Id == id);
             return book;
         }
 
@@ -62,17 +42,18 @@ namespace WebApi.Controllers
         [HttpPost]
         public IActionResult AddBook([FromBody] Book newBook)
         {
-            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            var book = _context.Books.SingleOrDefault(x => x.Title == newBook.Title);
             if(book is not null)
                 return BadRequest();
-            BookList.Add(newBook);
+            _context.Books.Add(newBook);
+            _context.SaveChanges();
             return Ok();
         }
 
         [HttpPut("{id}")]
         public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
         {
-            var book = BookList.SingleOrDefault(x => x.Id == id);
+            var book = _context.Books.SingleOrDefault(x => x.Id == id);
             if(book is  null)
                 return BadRequest();
             book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
@@ -80,17 +61,18 @@ namespace WebApi.Controllers
             book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
             book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
             book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
-
+            _context.SaveChanges();
             return Ok();
         }
 
         [HttpDelete("{id}")]
         public IActionResult DeleteBook(int id)
         {
-            var book = BookList.SingleOrDefault(x => x.Id == id);
+            var book = _context.Books.SingleOrDefault(x => x.Id == id);
             if(book is null)
                 return BadRequest();
-            BookList.Remove(book);
+            _context.Books.Remove(book);
+            _context.SaveChanges();
             return Ok();
         }
     }
