fb989059792775755cd96d58cf5434bb73ef3928
Onur Topal <onurtopal.duba@gmail.com>
2022-04-27 16:39:42 +0300

    yarıda kaldı

--- WebApi/BookOperations/GetBooks/GetBooksQuery.cs
+++ WebApi/BookOperations/GetBooks/GetBooksQuery.cs
@@ -2,6 +2,8 @@ using System.Linq;
 using System.Collections.Generic;
 using WebApi.DbOperationOptions;
 using WebApi.Models;
+using System;
+using WebApi.Common;
 
 namespace WebApi.BookOperations.GetBooks
 {
@@ -13,15 +15,28 @@ namespace WebApi.BookOperations.GetBooks
             _context = context;
         }
 
-        public List<Book> Handle()
+        public List<BooksViewModel> Handle()
         {
             var bookList = _context.Books.OrderBy(x => x.Id).ToList<Book>();
-            return bookList;
+            List<BooksViewModel> vm = new List<BooksViewModel>();
+            foreach (var book in bookList)
+            {   
+                vm.Add( new BooksViewModel {
+                    Title = book.Title,
+                    Genre = ((GenreEnum)book.GenreId).ToString(),
+                    PublishedDate = book.PublishDate.Date.ToString("dd/MM/yyyy"),
+                    PageCount = book.PageCount
+                });
+            }
+            return vm;
         }
     }
 
     public class BooksViewModel 
     {
-        public int MyProperty { get; set; }
+        public string Title { get; set; }
+        public int PageCount { get; set; }
+        public string PublishedDate { get; set; }
+        public string Genre { get; set; }
     }
 }
